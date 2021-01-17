cadab5bd40f593f390cc6f53e48b2930530e9cfe
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-01-17 21:31:34 +0900

    mongoDB

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,15 +1,32 @@
-import { query } from "express";
+import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => res.render("home", { pageTitle: "Home", potato: 1245 });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", {pageTitle: "Home", videos });
+  } catch {
+    console.log(error);
+    res.render("home", {pageTitle: "Home", videos: [] });
+  }
+};
+
 export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
-export const videos = (req, res) => res.render("videos", { pageTitle: "Video"});
 
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload"});
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload"});
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  // To Do: Upload and save video
+  res.redirect(routes.videoDetail(324393));
+};
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail"});
 
